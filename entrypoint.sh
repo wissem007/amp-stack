07ec8f4e912765d018d5478c8901cd939ed838c0
wissem007 <alouiwiss@gmail.com>
2019-08-02 14:19:07 +0200

    initialize wordpress

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 set -euo pipefail
-creadis(){
-	echo "this project is created in devops fomation"
+wordpress_initilizing(){
+	if [[ -d "/var/www/html" ]]; then
+		cd /var/www
+		wget https://fr.wordpress.org/latest-fr_FR.zip
+		unzip latest-fr_FR.zip
+		cd wordpress
+		mv * ../html
+		echo "[OK] Wordpress seccessfly initilized"
+	else
+		echo "[NOTICE] Wordpress alrady initilized ..."
+	fi
 }
 if [[ "$1" == apache2* ]]; then
         echo ""
         echo "* SERVER INITIALIZING ... *"
         echo ""
-        creadis
+        wordpress_initilizing
 		echo ""
         echo "* SERVER STARTED SUCCESSFULY *"
         echo ""
